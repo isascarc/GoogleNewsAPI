c126c9f22857cb4e888f65beab0589a1b0e62cdc
isascarc <isascarch@gmail.com>
2023-05-02 00:08:57 +0300

    Update tests

--- TestAPI/UnitTest1.cs
+++ TestAPI/UnitTest1.cs
@@ -12,10 +12,16 @@ namespace TestAPI
         [Test]
         public void Test1()
         {
-            var t = GoogleNewsAPI.NewsApi.GetNews(".net").Result;
+            var t = GoogleNewsAPI.NewsApi.GetNews("דרעי","IL","HE").Result;
             Assert.That(t, Is.Not.Null);
-            var t2 = GoogleNewsAPI.NewsApi.GetNews("").Result;
+            var t2 = GoogleNewsAPI.NewsApi.GetNews("bibi").Result;
             Assert.That(t2, Is.Not.Null);
+            var t3 = GoogleNewsAPI.NewsApi.GetNews("bibi","IL","HE").Result;
+            Assert.That(t3, Is.Not.Null);
+            var t4 = GoogleNewsAPI.NewsApi.GetNews("bibi","IL","EN").Result;
+            Assert.That(t4, Is.Not.Null);
+            var t5 = GoogleNewsAPI.NewsApi.GetNews("bibi","IL","RU").Result;
+            Assert.That(t5, Is.Not.Null);
         }
     }
 }
